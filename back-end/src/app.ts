c6d9ce4823f63d78ff1dc25c86b0998f48936e2f
nagilosantos <nagilosantos@gmail.com>
2023-07-31 01:55:00 -0300

    FIX/ adding cors on routs

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -5,8 +5,13 @@ import { handleAppError } from "./middlewares/haldleAppError.middleware";
 import { userRoutes } from "./routes/userRoute";
 import { loginRoutes } from "./routes/loginRoute";
 import { contactRoutes } from "./routes/contactRoute";
+import cors from 'cors';
+
 
 const app = express();
+
+app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: '*' }));
 
 app.use(express.json());
 app.use("/user", userRoutes);
